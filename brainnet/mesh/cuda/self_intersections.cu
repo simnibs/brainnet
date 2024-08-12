85679546c433dfcb076fe4e519b86d66c165d585
Jesper Duemose Nielsen <jesperdn@drcmr.dk>
2024-08-12 10:39:57 +0200

    switched to int32 in cuda extension

--- brainnet/mesh/cuda/self_intersections.cu
+++ brainnet/mesh/cuda/self_intersections.cu
@@ -285,20 +285,20 @@ __device__ float atomicMaxFloat(float* address, float val)
 __global__ void ProcessFaceKernel(
     const float* __restrict__ vertices,
     const size_t num_faces,
-    const int64_t* __restrict__ faces,
+    const int32_t* __restrict__ faces,
     float* __restrict__ faces_center,
     float* __restrict__ max_distance)
 {
-    int64_t face_vertices[3];
+    int32_t face_vertices[3];
     float center_position[3];
     float vertex_position[3];
     float thread_max_square_distance = 0;
 
     __shared__ float shared_distances[256];
 
-    const int64_t chunks = (1 + (num_faces - 1) / blockDim.x);
-    for (int64_t chunk = blockIdx.x; chunk < chunks; chunk += gridDim.x) {
-        const int64_t current_face_index = blockDim.x * chunk + threadIdx.x;
+    const int32_t chunks = (1 + (num_faces - 1) / blockDim.x);
+    for (int32_t chunk = blockIdx.x; chunk < chunks; chunk += gridDim.x) {
+        const int32_t current_face_index = blockDim.x * chunk + threadIdx.x;
         if (current_face_index >= num_faces) continue;
 
         size_t offset = current_face_index * 3;
@@ -342,33 +342,33 @@ __global__ void ProcessFaceKernel(
 }
 
 
-__global__ void MarkIntersectionKernel(
+__global__ void MarkSelfIntersectionKernel(
     const float* __restrict__ vertices,
     const size_t num_faces,
-    const int64_t* __restrict__ faces,
+    const int32_t* __restrict__ faces,
     const float* __restrict__ faces_center,
     const float* __restrict__ max_distance,
     int* __restrict__ faces_intersecting,
     int* __restrict__ num_intersecting)
 {
-    int64_t face_vertices[3];
+    int32_t face_vertices[3];
     float   face_position[3];
-    int64_t neighbor_face_vertices[3];
+    int32_t neighbor_face_vertices[3];
     float   neighbor_face_position[3];
 
     const float threshold = *max_distance;
 
-    const int64_t chunks = (1 + (num_faces - 1) / blockDim.x);
-    for (int64_t chunk = blockIdx.x; chunk < chunks; chunk += gridDim.x) {
+    const int32_t chunks = (1 + (num_faces - 1) / blockDim.x);
+    for (int32_t chunk = blockIdx.x; chunk < chunks; chunk += gridDim.x) {
 
-        const int64_t current_face_index = blockDim.x * chunk + threadIdx.x;
+        const int32_t current_face_index = blockDim.x * chunk + threadIdx.x;
         if (current_face_index >= num_faces) continue;
 
-        int64_t starting_face = current_face_index + 1;
+        int32_t starting_face = current_face_index + 1;
         VEC3_GET(face_vertices, faces, current_face_index * 3)
         VEC3_GET(face_position, faces_center, current_face_index * 3)
 
-        for (int64_t i = starting_face; i < num_faces; ++i) {
+        for (int32_t i = starting_face; i < num_faces; ++i) {
 
             VEC3_GET(neighbor_face_position, faces_center, i * 3)
             if (abs(neighbor_face_position[0] - face_position[0]) > threshold) continue;
@@ -392,10 +392,13 @@ __global__ void MarkIntersectionKernel(
                 faces_intersecting[i] = 1;
                 faces_intersecting[current_face_index] = 1;
                 atomicAdd(&num_intersecting[0], 1);
+                // current_face_index is the face currently being processed
+                // accumulate intersecting pairs instead of just an indicator (current_face_index, i)
             }
         }
     }
 }
+
 
 
 std::tuple<at::Tensor, at::Tensor> MarkSelfIntersectingFaces(
@@ -432,7 +435,7 @@ std::tuple<at::Tensor, at::Tensor> MarkSelfIntersectingFaces(
     ProcessFaceKernel<<<256, 256>>>(
         vertices.data_ptr<float>(),
         num_faces,
-        faces.data_ptr<int64_t>(),
+        faces.data_ptr<int32_t>(),
         faces_center.data_ptr<float>(),
         max_distance.data_ptr<float>());
 
@@ -440,10 +443,10 @@ std::tuple<at::Tensor, at::Tensor> MarkSelfIntersectingFaces(
     // each face (within the specified max distance) and check for intersections
     auto faces_intersecting = at::zeros({num_faces}, dtype_int);
     auto num_intersecting = at::zeros({1}, dtype_int);
-    MarkIntersectionKernel<<<256, 256, 0, stream>>>(
+    MarkSelfIntersectionKernel<<<256, 256, 0, stream>>>(
         vertices.data_ptr<float>(),
         num_faces,
-        faces.data_ptr<int64_t>(),
+        faces.data_ptr<int32_t>(),
         faces_center.data_ptr<float>(),
         max_distance.data_ptr<float>(),
         faces_intersecting.data_ptr<int>(),
@@ -452,3 +455,155 @@ std::tuple<at::Tensor, at::Tensor> MarkSelfIntersectingFaces(
     AT_CUDA_CHECK(cudaGetLastError());
     return std::make_tuple(faces_intersecting, num_intersecting);
 }
+
+// stuff for mesh-mesh intersection calculations
+
+// __global__ void MarkIntersectionKernel(
+//     const float* __restrict__ vertices_0,
+//     const float* __restrict__ vertices_1,
+//     const size_t num_faces_0,
+//     const size_t num_faces_1,
+//     const int32_t* __restrict__ faces_0,
+//     const int32_t* __restrict__ faces_1,
+//     const float* __restrict__ faces_center_0,
+//     const float* __restrict__ faces_center_1,
+//     const float* __restrict__ max_distance_0,
+//     const float* __restrict__ max_distance_1,
+//     int* __restrict__ faces_intersecting,
+//     int* __restrict__ num_intersecting,
+// )
+// {
+//     int32_t face_vertices[3];
+//     float   face_position[3];
+//     int32_t neighbor_face_vertices[3];
+//     float   neighbor_face_position[3];
+
+//     const float threshold = *max(max_distance_0, max_distance_1);
+
+//     const int32_t chunks = (1 + (num_faces - 1) / blockDim.x);
+//     for (int32_t chunk = blockIdx.x; chunk < chunks; chunk += gridDim.x) {
+
+//         const int32_t current_face_index = blockDim.x * chunk + threadIdx.x;
+//         if (current_face_index >= num_faces) continue;
+
+//         int32_t starting_face = current_face_index + 1;
+//         VEC3_GET(face_vertices, faces, current_face_index * 3)
+//         VEC3_GET(face_position, faces_center, current_face_index * 3)
+
+//         for (int32_t i = starting_face; i < num_faces; ++i) {
+
+//             VEC3_GET(neighbor_face_position, faces_center, i * 3)
+//             if (abs(neighbor_face_position[0] - face_position[0]) > threshold) continue;
+//             if (abs(neighbor_face_position[1] - face_position[1]) > threshold) continue;
+//             if (abs(neighbor_face_position[2] - face_position[2]) > threshold) continue;
+
+//             VEC3_GET(neighbor_face_vertices, faces, i * 3)
+//             if (VEC3_CONTAINS(neighbor_face_vertices, face_vertices[0]) ||
+//                 VEC3_CONTAINS(neighbor_face_vertices, face_vertices[1]) ||
+//                 VEC3_CONTAINS(neighbor_face_vertices, face_vertices[2])) continue;
+
+//             int intersect = triTriIntersect(
+//                 &vertices[face_vertices[0] * 3],
+//                 &vertices[face_vertices[1] * 3],
+//                 &vertices[face_vertices[2] * 3],
+//                 &vertices[neighbor_face_vertices[0] * 3],
+//                 &vertices[neighbor_face_vertices[1] * 3],
+//                 &vertices[neighbor_face_vertices[2] * 3]);
+
+//             if (intersect == 1) {
+//                 faces_intersecting[i] = 1;
+//                 faces_intersecting[current_face_index] = 1;
+//                 atomicAdd(&num_intersecting[0], 1);
+//                 // current_face_index is the face currently being processed
+//                 // accumulate intersecting pairs instead of just an indicator (current_face_index, i)
+//             }
+//         }
+//     }
+// }
+
+
+// std::tuple<at::Tensor, at::Tensor> MarkIntersectingFaces(
+//     const at::Tensor& vertices_0,
+//     const at::Tensor& faces_0,
+//     const at::Tensor& vertices_1,
+//     const at::Tensor& faces_1,
+// )
+// {
+//     at::TensorArg
+//         v0{vertices_0, "vertices_0",  1},
+//         f0{faces_0,    "faces_0",     2};
+//         v1{vertices_1, "vertices_1",  3},
+//         f1{faces_1,    "faces_1",     4};
+
+//     at::CheckedFrom c = "MarkSelfIntersectingFaces";
+//     at::checkAllSameGPU(c, {v0, f0, v1, f1});
+
+//     at::cuda::CUDAGuard device_guard(vertices_0.device());
+//     at::cuda::CUDAGuard device_guard(vertices_1.device());
+//     cudaStream_t stream = at::cuda::getCurrentCUDAStream();
+
+//     const auto num_vertices_0 = vertices_0.size(0);
+//     const auto num_vertices_1 = vertices_1.size(0);
+//     const auto num_faces_0 = faces_0.size(0);
+//     const auto num_faces_1 = faces_1.size(0);
+
+//     TORCH_CHECK(vertices_0.size(1) == 3, "Vertices must be 3D");
+//     TORCH_CHECK(vertices_1.size(1) == 3, "Vertices must be 3D");
+//     TORCH_CHECK(faces_0.size(1) == 3, "Faces must be triangular");
+//     TORCH_CHECK(faces_1.size(1) == 3, "Faces must be triangular");
+
+//     auto dtype_int   = vertices_0.options().dtype(at::kInt);
+//     auto dtype_float = vertices_0.options().dtype(at::kFloat);
+
+//     auto cvertices_0 = vertices_0.contiguous();
+//     auto cvertices_1 = vertices_1.contiguous();
+//     auto cfaces_0 = faces_0.contiguous();
+//     auto cfaces_1 = faces_1.contiguous();
+
+
+
+//     // pre-compute centers of each triangle for the nearest-neighbor
+//     // search and at the same time compute a reasonable max distance
+//     // threshold for ignoring faces that are too far
+//     auto faces_center_0 = at::zeros({num_faces_0, 3}, dtype_float);
+//     auto faces_center_1 = at::zeros({num_faces_1, 3}, dtype_float);
+//     auto max_distance_0 = at::zeros({1}, dtype_float);
+//     auto max_distance_1 = at::zeros({1}, dtype_float);
+
+//     ProcessFaceKernel<<<256, 256>>>(
+//         cvertices_0.data_ptr<float>(),
+//         num_faces_0,
+//         cfaces_0.data_ptr<int32_t>(),
+//         faces_center_0.data_ptr<float>(),
+//         max_distance_0.data_ptr<float>()
+//     );
+//     ProcessFaceKernel<<<256, 256>>>(
+//         cvertices_0.data_ptr<float>(),
+//         num_faces_0,
+//         cfaces_0.data_ptr<int32_t>(),
+//         faces_center_1.data_ptr<float>(),
+//         max_distance_1.data_ptr<float>()
+//     );
+
+//     // mark intersecting faces by finding a set of neighoring faces for
+//     // each face (within the specified max distance) and check for intersections
+//     auto faces_intersecting = at::zeros({num_faces}, dtype_int);
+//     auto num_intersecting = at::zeros({1}, dtype_int);
+//     MarkIntersectionKernel<<<256, 256, 0, stream>>>(
+//         cvertices_0.data_ptr<float>(),
+//         cvertices_1.data_ptr<float>(),
+//         num_faces_0,
+//         num_faces_1,
+//         cfaces_0.data_ptr<int32_t>(),
+//         cfaces_1.data_ptr<int32_t>(),
+//         faces_center_0.data_ptr<float>(),
+//         faces_center_1.data_ptr<float>(),
+//         max_distance_0.data_ptr<float>(),
+//         max_distance_1.data_ptr<float>(),
+//         faces_intersecting.data_ptr<int>(),
+//         num_intersecting.data_ptr<int>()
+//     );
+
+//     AT_CUDA_CHECK(cudaGetLastError());
+//     return std::make_tuple(faces_intersecting, num_intersecting);
+// }
