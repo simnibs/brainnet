3fd010a1f83305ef9c7130fc2ac48133b4eda01a
Jesper Duemose Nielsen <jesperdn@drcmr.dk>
2024-09-18 10:42:45 +0200

    added sbatch scripts for submitting training jobs

---
+++ tools/submit_brainnet.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+#SBATCH --job-name=brainnet         # Job name
+#SBATCH --output=/mnt/scratch/personal/jesperdn/slurm_logs/%x_%A_%a.log          # A = master job id, a = task job id
+#SBATCH --nodes=1                   # Relevant when program implements MPI (multi system/distributed parallelism)
+#SBATCH --ntasks=1                  # Relevant when program implements MPI (multi system/distributed parallelism)
+#SBATCH --cpus-per-task=8          # Relevant when program implements MP (single system parallelism, e.g., OpenMP, TBB)
+#SBATCH --mem=16G                   # Job memory request
+#SBATCH --gres=gpu:nvidia_geforce_rtx_3090:1
+
+echo "Job Information"
+echo
+echo "Job name     :  $SLURM_JOB_NAME"
+echo "Job ID       :  $SLURM_ARRAY_JOB_ID"
+echo "Task ID      :  $SLURM_ARRAY_TASK_ID"
+echo "Cluster name :  $SLURM_CLUSTER_NAME"
+echo "Node name    :  $SLURM_NODENAME"
+echo "Date         :  $(date)"
+echo "Working dir  :  $SLURM_SUBMIT_DIR"
+echo
+
+# By default, functions are not exported to be available in subshells so we
+# need this before we can use 'conda activate'
+source ~/mambaforge/etc/profile.d/conda.sh
+conda activate synth
+
+repo=/home/jesperdn/repositories/brainnet
+
+find $repo -name __pycache__ -exec rm -r {} +
+
+SCRIPT="${repo}/brainnet/train2.py"
+CONFIG="brainnet.config.surface_model.main"
+ARGS="--load-checkpoint 3000 --max-epochs 4000"
+# ARGS="--max-epochs 1000"
+
+cmd="python $SCRIPT $CONFIG $ARGS"
+
+echo
+echo "Executing : $cmd"
+echo
+
+$cmd
---
+++ tools/submit_brainreg.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+#SBATCH --job-name=brainreg         # Job name
+#SBATCH --output=/mnt/scratch/personal/jesperdn/slurm_logs/%x_%A_%a.log          # A = master job id, a = task job id
+#SBATCH --nodes=1                   # Relevant when program implements MPI (multi system/distributed parallelism)
+#SBATCH --ntasks=1                  # Relevant when program implements MPI (multi system/distributed parallelism)
+#SBATCH --cpus-per-task=8          # Relevant when program implements MP (single system parallelism, e.g., OpenMP, TBB)
+#SBATCH --mem=24G                   # Job memory request
+#SBATCH --gres=gpu:nvidia_geforce_rtx_3090:1
+
+echo "Job Information"
+echo
+echo "Job name     :  $SLURM_JOB_NAME"
+echo "Job ID       :  $SLURM_ARRAY_JOB_ID"
+echo "Task ID      :  $SLURM_ARRAY_TASK_ID"
+echo "Cluster name :  $SLURM_CLUSTER_NAME"
+echo "Node name    :  $SLURM_NODENAME"
+echo "Date         :  $(date)"
+echo "Working dir  :  $SLURM_SUBMIT_DIR"
+echo
+
+# By default, functions are not exported to be available in subshells so we
+# need this before we can use 'conda activate'
+source ~/mambaforge/etc/profile.d/conda.sh
+conda activate synth
+
+repo=/home/jesperdn/repositories/brainnet
+
+find $repo -name __pycache__ -exec rm -r {} +
+
+SCRIPT="${repo}/brainnet/train_brainreg.py"
+CONFIG="brainnet.config.brainreg.main"
+# ARGS="--load-checkpoint 400 --max-epochs 800"
+ARGS="--max-epochs 800"
+
+cmd="python $SCRIPT $CONFIG $ARGS"
+
+echo
+echo "Executing : $cmd"
+echo
+
+$cmd
